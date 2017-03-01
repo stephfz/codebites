2d2ff8427b97db136be06ccff774c9d47003a908
Stephanie Frias <stephaniefrias@Stephanies-MacBook-Pro-2.local>
2017-03-01 09:31:35 -0300

    test

--- amp_spike/read_and_seek.py
+++ amp_spike/read_and_seek.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-#path = "howto/templates/howto/guide.jinja"
+import os
 #path = "/Users/stephaniefrias/Projects/consumeraffairs/howto/templates/howto/guide.jinja"
 
-print sys.argv[1]
+#print sys.argv[1]
+print os.getcwd()
 
 search_term = 'amphtml'
 search_file = sys.argv[1]
 for line in open(search_file, 'r'):
     if re.search(search_term, line):
-        print 'jinja file {} modified. AMP template should modified as well'.format(search_file)
+        print 'jinja file {0} modified. AMP template should modified as well'.format(search_file)
